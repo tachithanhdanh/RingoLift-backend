2d2ad11acfb8f7f926533e537bad7dcf5a8a052c
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-11-03 11:39:02 +0700

    Initialize spring boot project

---
+++ src/main/java/com/gorgeous/ringolift/RingoliftApplication.java
@@ -0,0 +1,13 @@
+package com.gorgeous.ringolift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingoliftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingoliftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorgeous/ringolift/RingoliftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorgeous.ringolift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingoliftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
