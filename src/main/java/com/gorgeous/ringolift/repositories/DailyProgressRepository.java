e8bef4684e7daf471e02cd06d25fdf113f87820d
allforest01 <allforest01@gmail.com>
2024-11-23 22:29:38 +0700

    Add daily progress APIs

---
+++ src/main/java/com/gorgeous/ringolift/repositories/DailyProgressRepository.java
@@ -0,0 +1,8 @@
+package com.gorgeous.ringolift.repositories;
+
+import com.gorgeous.ringolift.models.DailyProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DailyProgressRepository extends JpaRepository<DailyProgress, Long> {
+
+}
