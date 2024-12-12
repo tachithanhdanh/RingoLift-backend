54ef0eebcdc3686ccc0c9d51bfe563fb0dfcb4e8
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-12-12 17:26:07 +0700

    Add Role model class and update User class for Auth

---
+++ src/main/java/com/gorgeous/ringolift/models/Role.java
@@ -0,0 +1,31 @@
+package com.gorgeous.ringolift.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 20)
+    private String name;
+
+    public static String ADMIN = "ADMIN";
+    public static String USER = "USER";
+}
