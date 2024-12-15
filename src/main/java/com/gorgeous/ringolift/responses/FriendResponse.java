b6dbb565e4d29006093432bb92778e37d2818c43
allforest01 <allforest01@gmail.com>
2024-12-15 17:02:54 +0700

    test

--- src/main/java/com/gorgeous/ringolift/responses/FriendResponse.java
+++ src/main/java/com/gorgeous/ringolift/responses/FriendResponse.java
@@ -1,5 +1,6 @@
 package com.gorgeous.ringolift.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gorgeous.ringolift.models.Friend;
 import lombok.*;
 
@@ -11,11 +12,22 @@ import java.time.LocalDateTime;
 @Builder
 public class FriendResponse extends BaseResponse {
 
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("sender_id")
     private Long senderId;
+
+    @JsonProperty("receiver_id")
     private Long receiverId;
+
+    @JsonProperty("status_type")
     private String statusType;
+
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
 
     public static FriendResponse fromFriend(Friend friend) {
